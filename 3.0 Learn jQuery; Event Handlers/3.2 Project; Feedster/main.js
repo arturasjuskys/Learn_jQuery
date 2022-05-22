1c39912db1a13dc19d79060fbe280afe16e18a63
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-05-22 11:12:28 +0100

    Adding - Finished Project Code

--- 3.0 Learn jQuery; Event Handlers/3.2 Project; Feedster/main.js
+++ 3.0 Learn jQuery; Event Handlers/3.2 Project; Feedster/main.js
@@ -1,3 +1,37 @@
 $(document).ready(() => {
-
-}); 
+  // 1.
+  $('.menu').on('mouseover', () => {
+    $('.nav-menu').show();
+  });
+  // 2.
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  });
+  // 3. & 5.
+  $('.btn')
+  .on('mouseover', event => {
+    $(event.currentTarget).addClass('btn-hover');
+  })
+  // 4. & 5.
+  .on('mouseleave', event => {
+    $(event.currentTarget).removeClass('btn-hover');
+  })
+  // 6. & 8.
+  $('.postText').on('keyup', event => {
+    // 7.
+    $('.postText').focus();
+    // 8.
+    const post = $(event.currentTarget).val();
+    // 9.
+    let remaining = 140 - post.length;
+    // 11.
+    if (remaining <= 0) {
+      $('.wordcount').addClass('red');
+    } else {
+      // 12.
+      $('.wordcount').removeClass('red');
+    }
+    // 10.
+    $('.characters').html(remaining);
+  })
+});
